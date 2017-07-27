3368c4ca6e59cbacb7a80a31d7b6d80240f2d3fc
CTETCHR1 <CTETCHR1@s-cte-tchr1.local>
2017-07-27 15:07:34 -0500

    comments

--- Clicky/ViewController.swift
+++ Clicky/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //This is a test comment
+        // This is the comment
         // Hello there
     }
 

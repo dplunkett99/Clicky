c832fa5fd2916f73b055afa1d795b567b6388636
Yamil Ivan Rodriguez <yamilivanrodriguez@yamils-macbook-pro.local>
2017-07-27 14:26:00 -0500

    added newPlayScreenViewController and attached IBActions and outlets

---
+++ Clicky/PlayScreenViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PlayScreenViewController.swift
+//  Clicky
+//
+//  Created by Yamil Ivan Rodriguez on 7/27/17.
+//  Copyright © 2017 DGS. All rights reserved.
+//
+
+import UIKit
+
+class PlayScreenViewController: UIViewController {
+
+    @IBOutlet weak var clickyButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func clickyButton(_ sender: UIButton) {
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

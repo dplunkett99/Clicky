78fbeff898e117bbe5d4b0df5c913c22a24c0c9c
Yamil Ivan Rodriguez <yamilivanrodriguez@yamils-macbook-pro.local>
2017-07-27 16:01:54 -0500

    added logic for block movement and timer start/stop

--- Clicky/PlayScreenViewController.swift
+++ Clicky/PlayScreenViewController.swift
@@ -16,28 +16,33 @@ class PlayScreenViewController: UIViewController {
     
     // implementation variables
     var successfulPresses = 0
-    let buttonWidth = 100
-    let buttonHeight = 100
+    let buttonWidth = 50
+    let buttonHeight = 50
     var buttonLocation = CGRect(x: 100, y: 100, width: 100, height: 100)
     var playTimer = Timer()
-    var leftBound = 0.0
-    var rightBound = 0.0
-    var topBound = 0.0
-    var lowerBound = 0.0
+    var leftBound = 0
+    var rightBound = 0
+    var topBound = 0
+    var lowerBound = 0
+    let timeLimit = 10 //in secs
+    var timeLeft = 10
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        buttonLocation = CGRect(x: 0, y: 250, width: buttonWidth, height: buttonHeight)
         clickyButton.frame = buttonLocation
+        timeLeft = timeLimit
+        timeLabel.text = "TimeLeft: " + String(timeLeft)
         
         // set screenbounds
         let screenSize = self.view.bounds
-        leftBound = Double(screenSize.minX)
-        rightBound = Double(screenSize.maxX)
-        topBound = Double(screenSize.minY)
-        lowerBound = Double(screenSize.maxY)
+        leftBound = Int(screenSize.minX)
+        rightBound = Int(screenSize.maxX)
+        topBound = Int(screenSize.minY)
+        lowerBound = Int(screenSize.maxY)
         
         
     }
@@ -45,12 +50,36 @@ class PlayScreenViewController: UIViewController {
     
     @IBAction func clickyButton(_ sender: UIButton) {
         
-        playTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateButtonLocation), userInfo: nil, repeats: true)
+        playTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(updateButtonLocation), userInfo: nil, repeats: true)
         
+        // update timer label
+        timeLabel.text = "TimeLeft: " + String(timeLeft)
+
     }
     
     func updateButtonLocation() {
         
+        // update location of button
+        let tmpX = arc4random_uniform(UInt32(rightBound - buttonWidth))
+        let tmpY = arc4random_uniform(UInt32(lowerBound - buttonHeight))
+        clickyButton.frame = CGRect(x: Int(tmpX), y: Int(tmpY), width: buttonWidth, height: buttonHeight)
+        
+        // make button visible or invisible 
+        if timeLeft % 2 == 0 {
+            clickyButton.backgroundColor = self.view.backgroundColor
+            clickyButton.isEnabled = false
+        }
+        else {
+            clickyButton.isEnabled = true
+            clickyButton.backgroundColor = UIColor.red
+        }
+        
+        timeLeft -= 1
+        
+        if timeLeft == 0 {
+            playTimer.invalidate()
+            timeLeft = timeLimit
+        }
     }
 
     override func didReceiveMemoryWarning() {
